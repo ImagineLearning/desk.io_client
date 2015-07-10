ff070fddb160618d11bb891c3c8c4727ee90fb14
Corey Larson <corey.larson@imaginelearning.com>
2015-07-10 13:07:48 -0600

    Finishing the WinForms desk control

--- DeskIOWindows/Form1.Designer.cs
+++ DeskIOWindows/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DeskIOWindows
 {
-	partial class Form1
+	partial class DeskForm
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,45 +28,91 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeskForm));
+			this.UpButton = new System.Windows.Forms.Button();
+			this.DownArrow = new System.Windows.Forms.Button();
+			this.IPAddressTextBox = new System.Windows.Forms.TextBox();
+			this.IPAddressLabel = new System.Windows.Forms.Label();
+			this.ConnectButton = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
-			// button1
+			// UpButton
 			// 
-			this.button1.Location = new System.Drawing.Point(121, 53);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
+			this.UpButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("UpButton.BackgroundImage")));
+			this.UpButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.UpButton.Enabled = false;
+			this.UpButton.Location = new System.Drawing.Point(12, 52);
+			this.UpButton.Name = "UpButton";
+			this.UpButton.Size = new System.Drawing.Size(150, 150);
+			this.UpButton.TabIndex = 0;
+			this.UpButton.UseVisualStyleBackColor = true;
+			this.UpButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.UpButton_MouseDown);
+			this.UpButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UpButton_MouseUp);
 			// 
-			// button2
+			// DownArrow
 			// 
-			this.button2.Location = new System.Drawing.Point(121, 82);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 1;
-			this.button2.Text = "button2";
-			this.button2.UseVisualStyleBackColor = true;
+			this.DownArrow.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("DownArrow.BackgroundImage")));
+			this.DownArrow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.DownArrow.Enabled = false;
+			this.DownArrow.Location = new System.Drawing.Point(12, 208);
+			this.DownArrow.Name = "DownArrow";
+			this.DownArrow.Size = new System.Drawing.Size(150, 150);
+			this.DownArrow.TabIndex = 1;
+			this.DownArrow.UseVisualStyleBackColor = true;
+			this.DownArrow.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DownArrow_MouseDown);
+			this.DownArrow.MouseUp += new System.Windows.Forms.MouseEventHandler(this.DownArrow_MouseUp);
 			// 
-			// Form1
+			// IPAddressTextBox
+			// 
+			this.IPAddressTextBox.Location = new System.Drawing.Point(12, 26);
+			this.IPAddressTextBox.Name = "IPAddressTextBox";
+			this.IPAddressTextBox.Size = new System.Drawing.Size(88, 20);
+			this.IPAddressTextBox.TabIndex = 2;
+			// 
+			// IPAddressLabel
+			// 
+			this.IPAddressLabel.AutoSize = true;
+			this.IPAddressLabel.Location = new System.Drawing.Point(9, 9);
+			this.IPAddressLabel.Name = "IPAddressLabel";
+			this.IPAddressLabel.Size = new System.Drawing.Size(48, 13);
+			this.IPAddressLabel.TabIndex = 3;
+			this.IPAddressLabel.Text = "Desk IP:";
+			// 
+			// ConnectButton
+			// 
+			this.ConnectButton.Location = new System.Drawing.Point(106, 24);
+			this.ConnectButton.Name = "ConnectButton";
+			this.ConnectButton.Size = new System.Drawing.Size(56, 23);
+			this.ConnectButton.TabIndex = 4;
+			this.ConnectButton.Text = "Connect";
+			this.ConnectButton.UseVisualStyleBackColor = true;
+			this.ConnectButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ConnectButton_MouseClick);
+			// 
+			// DeskForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 261);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Name = "Form1";
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(175, 366);
+			this.Controls.Add(this.ConnectButton);
+			this.Controls.Add(this.IPAddressLabel);
+			this.Controls.Add(this.IPAddressTextBox);
+			this.Controls.Add(this.DownArrow);
+			this.Controls.Add(this.UpButton);
+			this.Name = "DeskForm";
+			this.Text = "Desk";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.DeskForm_FormClosed);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button UpButton;
+		private System.Windows.Forms.Button DownArrow;
+		private System.Windows.Forms.TextBox IPAddressTextBox;
+		private System.Windows.Forms.Label IPAddressLabel;
+		private System.Windows.Forms.Button ConnectButton;
 	}
 }
 
--- DeskIOWindows/Form1.cs
+++ DeskIOWindows/Form1.cs
@@ -1,20 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
 using System.Windows.Forms;
+using DeskIOLib;
 
 namespace DeskIOWindows
 {
-	public partial class Form1 : Form
+	public partial class DeskForm : Form
 	{
-		public Form1()
+
+		private Desk _desk;
+
+		public DeskForm()
 		{
 			InitializeComponent();
+		}
+
+		private void UpButton_MouseDown(object sender, MouseEventArgs e)
+		{
+			_desk.Raise();
+		}
+
+		private void UpButton_MouseUp(object sender, MouseEventArgs e)
+		{
+			_desk.Stop();
+		}
+
+		private void DownArrow_MouseDown(object sender, MouseEventArgs e)
+		{
+			_desk.Lower();
+		}
+
+		private void DownArrow_MouseUp(object sender, MouseEventArgs e)
+		{
+			_desk.Stop();
+		}
+
+		private void ConnectButton_MouseClick(object sender, MouseEventArgs e)
+		{
+			IPAddress deskAddress;
+
+			if (!IPAddress.TryParse(IPAddressTextBox.Text, out deskAddress))
+			{
+				MessageBox.Show("Invalid IP address entered", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			_desk = new Desk(deskAddress);
+			DownArrow.Enabled = true;
+			UpButton.Enabled = true;
+		}
+
+		private void DeskForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			if(_desk != null)
+				_desk.Disconnect();
 		}
 	}
 }
