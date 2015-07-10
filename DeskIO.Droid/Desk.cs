e626b46f521474b11f91a62a0b73ae94cc57efb0
Corey Larson <corey.larson@imaginelearning.com>
2015-07-10 15:27:31 -0600

    Add android app

---
+++ DeskIO.Droid/Desk.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DeskIO.Droid
+{
+	public class Desk
+	{
+		public const int DeskIOSocketPort = 8000;
+		public IPAddress DeskControllerIpAddress { get; private set; }
+
+		private Socket _deskSocket;
+
+		public Desk(IPAddress deskControllerIpAddress)
+		{
+			DeskControllerIpAddress = deskControllerIpAddress;
+
+			_deskSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			_deskSocket.Connect(DeskControllerIpAddress, DeskIOSocketPort);
+		}
+
+		public void Raise(int duration = -1)
+		{
+			MoveDesk(MovementDirection.Up, duration);
+		}
+
+		public void Lower(int duration = -1)
+		{
+			MoveDesk(MovementDirection.Down, duration);
+		}
+
+		private void MoveDesk(MovementDirection direction, int movementDuration)
+		{
+			var commandString = direction.ToString();
+
+			if (movementDuration > 0)
+				commandString += " " + movementDuration;
+
+			var command = Encoding.UTF8.GetBytes(commandString);
+
+			_deskSocket.Send(command);
+		}
+
+		public void Stop()
+		{
+			var command = Encoding.UTF8.GetBytes("Stop");
+			_deskSocket.Send(command);
+		}
+
+		public void Disconnect()
+		{
+			_deskSocket.Shutdown(SocketShutdown.Both);
+			_deskSocket.Close();
+		}
+	}
+
+	public enum MovementDirection
+	{
+		Up,
+		Down
+	}
+}
---
+++ DeskIO.Droid/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DeskIO.Droid
+{
+	[Activity (Label = "DeskIO", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			EditText ipBox = FindViewById<EditText> (Resource.Id.ipEditBox);
+
+			Button upButton = FindViewById<Button> (Resource.Id.upButton);
+			upButton.Click += (object sender, EventArgs e) => {
+				if(string.IsNullOrEmpty(ipBox.Text)) {
+					return;
+				}
+
+				IPAddress deskAddress;
+				if(!IPAddress.TryParse(ipBox.Text, out deskAddress)) {
+					return;
+				}
+
+				var desk = new Desk(deskAddress);
+
+				if(upButton.Text == "Stop") {
+					desk.Stop();
+					upButton.Text = "Up";
+					return;
+				}
+
+				upButton.Text = "Stop";
+				desk.Raise();
+				desk.Disconnect();
+			};
+
+			Button downButton = FindViewById<Button> (Resource.Id.downButton);
+			downButton.Click += (object sender, EventArgs e) => {
+				if(string.IsNullOrEmpty(ipBox.Text)) {
+					return;
+				}
+
+				IPAddress deskAddress;
+				if(!IPAddress.TryParse(ipBox.Text, out deskAddress)) {
+					return;
+				}
+
+				var desk = new Desk(deskAddress);
+
+				if(downButton.Text == "Stop") {
+					desk.Stop();
+					downButton.Text = "Down";
+					return;
+				}
+
+				downButton.Text = "Stop";
+				desk.Lower();
+				desk.Disconnect();
+			};
+		}
+	}
+}
+
+
